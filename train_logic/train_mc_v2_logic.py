94fd1789b8919c03b0258849317075a554f7a2c7
yo-sarthak <yosarthakyay@gmail.com>
2025-05-15 15:06:57 +0200

    Update train_mc_v2_logic.py
    Removed reward for terminate=true since environment already uses it

--- train_logic/train_mc_v2_logic.py
+++ train_logic/train_mc_v2_logic.py
@@ -41,10 +41,6 @@ def train_mc_control(agent, state, env, iters, max_diff_list, delta, episode, ep
         # The action is performed in the environment
         next_state, reward, terminated, info = env.step(action)
 
-        # Give additional reward on reaching the end goal, minus the steps it took to reach, encouraging the agent to reach faster
-        if terminated:
-            reward = 1_000
-
         # Update episode-history
         agent.update(state, reward, info["actual_action"])
 
